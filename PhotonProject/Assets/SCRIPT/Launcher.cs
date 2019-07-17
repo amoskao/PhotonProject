28a26aadb60ac7497dfe2d408cfd8aaf5b035545
amoskao <51389193+amoskao@users.noreply.github.com>
2019-07-17 16:03:23 +0800

    完成連線，大廳跟遊戲場景，建立房間與加入房間

---
+++ PhotonProject/Assets/SCRIPT/Launcher.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    [Header("輸出文字")]
+    public Text textPrint;
+
+    [Header("輸入欄位")]
+    public InputField PlayerIF;
+    public InputField roomCreateIF;
+    public InputField roomJoinIF;
+    public Button BtnCreate, BtnJoin;
+    
+    public string namePlayer, nameCreateRoom, nameJoinRoom;
+
+    public string NamePlayer { get => namePlayer; set => namePlayer = value; }
+    public string NameCreateRoom { get => nameCreateRoom; set => nameCreateRoom = value; }
+    public string NameJoinRoom { get => nameJoinRoom; set => nameJoinRoom = value; }
+
+    private void Start()
+    {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        
+
+    }
+
+    public void BtnCreateRoom()
+    {
+        PhotonNetwork.CreateRoom(NameCreateRoom, new RoomOptions { MaxPlayers = 20 });
+    }
+    public void BtnJoinRoom()
+    {
+        PhotonNetwork.JoinRoom(NameJoinRoom);
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        textPrint.text = "連線成功!";
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        textPrint.text = "已進入大廳!";
+        PlayerIF.interactable = true;
+        roomCreateIF.interactable = true;
+        roomJoinIF.interactable = true;
+        BtnCreate.interactable = true;
+        BtnJoin.interactable = true;
+    }
+
+    public override void OnCreatedRoom()
+    {
+        textPrint.text = "已建立房間名：" + NameCreateRoom;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        textPrint.text = "已加入房間名：" + NameJoinRoom;
+        PhotonNetwork.LoadLevel("遊戲場景");
+
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        textPrint.text = "加入房間失敗,Code：" + returnCode + "訊息:"+ message;
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        textPrint.text = "加入房間失敗,Code：" + returnCode + "訊息:" + message;
+    }
+
+}
