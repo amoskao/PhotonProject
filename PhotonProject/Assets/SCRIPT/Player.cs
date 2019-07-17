1baed47b572afddf3c45b7fa7051b6f25618f376
amoskao <51389193+amoskao@users.noreply.github.com>
2019-07-17 16:36:24 +0800

    新增移動

---
+++ PhotonProject/Assets/SCRIPT/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace amos
+{
+    public class Player : MonoBehaviour
+    {
+        public Rigidbody2D rig;
+        public float speed = 10;
+        private void FixedUpdate()
+        {
+            Move();
+        }
+        private void Move()
+        {
+            rig.AddForce((
+                transform.right * Input.GetAxisRaw("Horizontal") + 
+                transform.up * Input.GetAxisRaw("Vertical"))*speed);
+        }
+    }
+}
