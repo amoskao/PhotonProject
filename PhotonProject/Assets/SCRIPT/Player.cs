352bafc51122118bb010d3f406bb257c64024ca6
amoskao <51389193+amoskao@users.noreply.github.com>
2019-07-24 11:47:58 +0800

    新增同步更新座標連線完善

--- PhotonProject/Assets/SCRIPT/Player.cs
+++ PhotonProject/Assets/SCRIPT/Player.cs
@@ -1,20 +1,80 @@
 ﻿using UnityEngine;
-
+using Photon.Pun;
 namespace amos
 {
-    public class Player : MonoBehaviour
+    public class Player : MonoBehaviourPun,IPunObservable
     {
+        [Header("剛體")]
         public Rigidbody2D rig;
+        [Header("速度")]
         public float speed = 10;
+
+        [Header("Photon元件")]
+        public PhotonView pv;
+
+        [Header("Player腳本")]
+        public Player player;
+
+        [Header("攝影機")]
+        public GameObject obj;
+        [Header("同步座標資訊")]
+        public Vector3 positionNext;
+        [Header("同步平滑速度"), Range(0.1f, 5f)]
+        public float smoothSpeed = 0.5f;
+
+        private void Start()
+        {
+            //如果 不是自己的物件
+            if (!pv.IsMine)
+            {
+               //player.enabled = false; //玩家元件=關閉
+                obj.SetActive(false);   //攝影機物件(關閉)
+
+            }
+        }
         private void FixedUpdate()
         {
-            Move();
+            if (pv.IsMine)
+            {
+                Move();
+            }
+            else
+            {
+                SmoohtMove();
+            }
         }
+
+
+        /// <summary>
+        /// 其他玩家的物件同步平滑移動
+        /// </summary>
+        private void SmoohtMove()
+        {
+            //其他玩家的座標=插植(原本的座標，同步座標資訊，百分比-同步平滑速度*一個影格的時間)
+            transform.position = Vector3.Lerp(transform.position, positionNext, smoothSpeed * Time.deltaTime);//乘上Time.deltaTime用意是讓移動效果更好 官方建議使用
+        }
+
         private void Move()
         {
+
             rig.AddForce((
                 transform.right * Input.GetAxisRaw("Horizontal") + 
                 transform.up * Input.GetAxisRaw("Vertical"))*speed);
         }
+
+        //同步資料方法( Player : MonoBehaviourPun,IPunObservable寫好後按燈泡 實作  刪掉裡面那行程式)
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                stream.SendNext(transform.position);            //傳遞資料(座標)
+            }
+            else if (stream.IsReading)
+            {
+                positionNext = (Vector3)stream.ReceiveNext();   //同步座標資訊=(轉型)接收資料
+            }
+        }
+
+                    
     }
 }
---
+++ PhotonProject/Assets/Script/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Photon.Pun;
+public class GameManager : MonoBehaviour
+{
+    [Header("玩家預製物")]
+    public GameObject prefabPlayer;
+    [Header("生成座標")]
+    public Transform[] spawnPoints;
+
+    private void SpawnPlayer()
+    {
+        //隨機= 隨機(0，生成座標陣列的長度)、(0，6)結果為0~5隨機值
+        int r = Random.Range(0, spawnPoints.Length);
+        //photon連線.實例化(物件名稱，座標，角度) Quaternion.identity= 沒有角度
+        PhotonNetwork.Instantiate(prefabPlayer.name, spawnPoints[r].position, Quaternion.identity);
+    }
+    private void Start()
+    {
+        SpawnPlayer();
+    }
+}
